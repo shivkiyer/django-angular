7adab1ffd974acb0d09455d5ae0b3ecfe5ce0e01
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2018-07-26 22:55:27 -0400

    Started Angular form and Django class views.

--- frontend/src/app/new-company/new-company.component.ts
+++ frontend/src/app/new-company/new-company.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { environment } from './../../environments/environment';
 
@@ -11,6 +12,17 @@ import { environment } from './../../environments/environment';
 export class NewCompanyComponent implements OnInit {
 
   apiBaseURL: string = environment.configSettings.apiURL;
+  response: string;
+  newCompanyForm: FormGroup = new FormGroup({
+    'name': new FormControl(),
+    'headquarters': new FormControl(),
+    'address': new FormControl(),
+    'company_website': new FormControl(),
+    'established_year': new FormControl(null, [Validators.required,
+                                                Validators.min(1900),
+                                                Validators.max(2018)])
+  });
+  displayForm: boolean = false;
 
   constructor(private http: Http) { }
 
@@ -22,11 +34,20 @@ export class NewCompanyComponent implements OnInit {
       ).subscribe(
         (response) => {
           console.log(response);
+          this.response = response.json().message;
         },
         (errors) => {
           console.log(errors);
         }
       )
+  }
+
+  emptyForm() {
+    this.displayForm = true;
+  }
+
+  addCompany() {
+    console.log(this.newCompanyForm.value);
   }
 
 }
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict
+from django.views import View
 
 from .forms import CompanyForm, EmployeeForm
 from .models import Company, Employee, SomeModel
@@ -8,16 +9,7 @@ from .serializers import SomeModelSerializer
 
 # Create your views here.
 
-def new_company(request):
-    # new_form = CompanyForm()
-    # new_object = Company()
-    # new_object_as_dict = model_to_dict(new_object)
-    # del new_object_as_dict['id']
-    # new_somemodel = SomeModelSerializer()
-    # print(new_somemodel)
-    # print()
-    # print(new_somemodel.serializer_field_mapping)
-    # print()
-    # print(dir(new_somemodel))
-    # return HttpResponse(new_somemodel)
-    return HttpResponse("Hello World")
+class NewCompany(View):
+    def get(self, request):
+        # return HttpResponse('Hello world')
+        return JsonResponse({"message": "Hello world"})
